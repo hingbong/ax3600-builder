6c69de8ebfd33bdd4fc06a225f28008e0b7af1aa
hingbong💫 <hingbonglo@gmail.com>
2023-06-08 20:49:47 +0800

    use luci-default instead of luci-nginx

--- openwrt.sh
+++ openwrt.sh
@@ -12,11 +12,6 @@ grep "10.0.0.1" package/base-files/files/bin/config_generate
 sed -i 's/.ssid=OpenWrt/.ssid=OpenWrt_AX3600/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 grep "OpenWrt_AX3600" package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# Listen with HTTP/2
-sed -i "s/ssl default_server/ssl default_server http2/g" feeds/packages/net/nginx-util/files/nginx.config
-grep "list listen" feeds/packages/net/nginx-util/files/nginx.config
-
-
 #NTP server
 sed -i "s/0.openwrt.pool.ntp.org/ntp.tuna.tsinghua.edu.cn/g" package/base-files/files/bin/config_generate
 sed -i "/add_list system.ntp.server='1.openwrt.pool.ntp.org'/d" package/base-files/files/bin/config_generate
