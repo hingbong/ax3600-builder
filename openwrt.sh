885d8ac7e0931644a7b9904cbe7b41646f79f4cb
hingbong💫 <hingbonglo@gmail.com>
2023-08-12 20:28:24 +0800

    update

--- openwrt.sh
+++ openwrt.sh
@@ -13,8 +13,10 @@ sed -i "/add_list system.ntp.server='2.openwrt.pool.ntp.org'/d" package/base-fil
 sed -i "/add_list system.ntp.server='3.openwrt.pool.ntp.org'/d" package/base-files/files/bin/config_generate
 grep "system.ntp.server" package/base-files/files/bin/config_generate
 
-sed -i "s#git.openwrt.org/feed#github.com/openwrt#g " feeds.conf.default
-sed -i "s#git.openwrt.org/project#github.com/openwrt#g " feeds.conf.default
+sed -i "s#git.openwrt.org/feed#github.com/openwrt#g" feeds.conf.default
+sed -i "s#git.openwrt.org/project#github.com/openwrt#g" feeds.conf.default
+
+echo "src-git mosdns https://github.com/hingbong/luci-app-mosdns.git" >> feeds.conf.default
 
 cat feeds.conf.default
 
