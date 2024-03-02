72147168da3b07fb2c22b07c89c931e750207692
hingbong💫 <hingbonglo@gmail.com>
2024-03-02 16:16:45 +0000

    cleanup openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -2,10 +2,6 @@
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 grep "10.0.0.1" package/base-files/files/bin/config_generate
 
-# Default SSID
-sed -i 's/.ssid=OpenWrt/.ssid=OpenWrt_AX3600/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-grep "OpenWrt_AX3600" package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
 #NTP server
 sed -i "s/0.openwrt.pool.ntp.org/ntp.tuna.tsinghua.edu.cn/g" package/base-files/files/bin/config_generate
 sed -i "/add_list system.ntp.server='1.openwrt.pool.ntp.org'/d" package/base-files/files/bin/config_generate
